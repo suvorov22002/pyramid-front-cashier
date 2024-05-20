68b75438048f0d725afb7d5b891b5fb1eb3fbeb6
suvorov <suvorov22002@gmail.com>
2024-05-20 22:34:52 +0200

    design du dashboard et checkin des infos tickets

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  firstForm: UntypedFormGroup;
+  radioGroupValue: any = 'No';
+  AllGames: string[] = ["keno","bingo","spin","dog","soccer"];
+
+  constructor(private fb: UntypedFormBuilder) {}
+  
+  ngOnInit(): void {
+    this.firstForm = this.fb.group({
+      amount: [null, [Validators.required]],
+      coefficient: ['No'],
+    });
+  }
+
+  onSubmitBet() {
+
+  }
+
+  onCancelBet(){
+
+  }
+
+  validateSubmit(){
+    return false;
+  }
+
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
-import { NbCardModule } from '@nebular/theme';
+import { NbActionsModule, NbButtonModule, NbCardModule, NbCheckboxModule, NbDatepickerModule, NbIconModule, NbInputModule, NbListModule, NbRadioModule, NbSelectModule, NbTabsetModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const formulaireModule = [
+  NbInputModule,
+  NbActionsModule,
+  NbCheckboxModule,
+  NbRadioModule,
+  NbDatepickerModule,
+  NbSelectModule,
+  NbIconModule,
+  NbButtonModule
+];
+
 
 @NgModule({
   imports: [
     NbCardModule,
     ThemeModule,
+    NbTabsetModule,
+    NbListModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...formulaireModule
   ],
   declarations: [
     DashboardComponent,
